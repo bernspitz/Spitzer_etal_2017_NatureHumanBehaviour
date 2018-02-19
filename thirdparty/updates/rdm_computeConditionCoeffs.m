a4e8821bd926b5a166e23ca8c62779707d678c5e
Spitzer <spitzer@ARC-OSX-004252.local>
2018-02-19 12:22:04 +0100

    update commit

--- thirdparty/updates/rdm_computeConditionCoeffs.m
+++ thirdparty/updates/rdm_computeConditionCoeffs.m
@@ -1,34 +1,32 @@
-    function results = rdm_computeConditionCoeffs(eegMat,behavVect)
-    %% RDM_COMPUTECONDITIONCOEFFS
-    %
-    % regresses the eeg signal on the trial-by-condition design matrix for all electrodes and points in time
-    %
-    % eegMat = electrode-by-time-by-trial
-    % behavVect = trial-by-1 (indicating the condition on each trial)
-    %
-    % (c) Timo Flesch, 2016
-     
-    % set up design matrix
-    conditions = unique(behavVect)';
-    dmat = zeros(size(behavVect,1),length(conditions));
-    for c = 1:length(conditions)
-        dmat(behavVect==conditions(c),c) = 1;   
-    end
-    % add constant term
-    dmat = [ones(size(dmat,1),1) dmat];
-    
-    results = struct(); 
-    results.betas   = zeros(size(eegMat,1),size(eegMat,2),length(conditions));
-    results.resids  = zeros(size(eegMat));
-     
-    % for all electrodes and time points: run regression
-    for el = 1:size(eegMat,1)
-        %el
-        for t = 1:size(eegMat,2)
-           [betas,~,resid] = regress(zscore(squeeze(eegMat(el,t,:))),dmat);
-           results.betas(el,t,:) = betas(2:end); % drop constant term
-           results.resids(el,t,:) = resid;
-        end       
-    end
-    end
+function results = rdm_computeConditionCoeffs(eegMat,behavVect)
+%% RESULTS = RDM_COMPUTECONDITIONCOEFFS(EEGMAT,BEHAVVECT)
+%
+% regresses the eeg signal on the trial-by-condition design matrix for all electrodes and points in time
+%
+% eegMat = electrode-by-time-by-trial matrix of raw voltages
+% behavVect = trial-vector (indicating the condition on each trial)
+%
+% (c) Timo Flesch, 2016
+ 
+results = struct(); 
+results.betas   = [];
+results.resids  = [];
+ 
+% set up design matrix
+conditions = unique(behavVect)';
+dmat = zeros(size(behavVect,1),length(conditions));
+for c = 1:length(conditions)
+    dmat(behavVect==conditions(c),c) = 1;   
+end
+ 
+ 
+% for all electrodes and time points: run regression
+for el = 1:size(eegMat,1)
+    for t = 1:size(eegMat,2)
+       [betas,~,resid] = regress(zscore(squeeze(eegMat(el,t,:))),dmat);
+       results.betas(el,t,:) = betas; 
+       results.resids(el,t,:) = resid;
+    end       
+end
+end
 
